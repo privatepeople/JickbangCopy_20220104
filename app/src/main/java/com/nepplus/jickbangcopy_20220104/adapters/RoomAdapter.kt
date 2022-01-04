dc91317d1983ee5609562aad5fe3ecbd03cc36d8
Lee Seung Hun <negrat@naver.com>
2022-01-04 15:02:36 +0900

    class RoomAdapter 라인의 에러 해결     - 3개 함수 내부 구현해야함

--- app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -1,11 +1,24 @@
 package com.nepplus.jickbangcopy_20220104.adapters
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class RoomAdapter : RecyclerView.Adapter<>() {
+class RoomAdapter : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
 
     }
 

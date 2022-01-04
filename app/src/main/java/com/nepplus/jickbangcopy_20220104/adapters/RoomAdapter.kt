ba674ccd789b0b399a2138b3c8245a55d15552f3
Lee Seung Hun <negrat@naver.com>
2022-01-04 15:08:10 +0900

    RoomAdapter 기초 뼈대작업 마무리

--- app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -1,16 +1,27 @@
 package com.nepplus.jickbangcopy_20220104.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.nepplus.jickbangcopy_20220104.R
+import com.nepplus.jickbangcopy_20220104.models.RoomData
+import java.math.MathContext
 
-class RoomAdapter : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
+class RoomAdapter(
+    val mathContext: Context,
+    val mList: List<RoomData>
+    ) : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mathContext).inflate(R.layout.room_list_item, parent, false)
+        return MyViewHolder(row)
 
     }
 
@@ -19,6 +30,8 @@ class RoomAdapter : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
     }
 
     override fun getItemCount(): Int {
+
+        return mList.size
 
     }
 

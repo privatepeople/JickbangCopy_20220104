480a336d56680f12e1e4dadaaff165a8487a17ae
Lee Seung Hun <negrat@naver.com>
2022-01-04 16:34:32 +0900

    어댑터 안에서 -> 상세화면으로 이동     - Intent 사용

--- app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -1,6 +1,7 @@
 package com.nepplus.jickbangcopy_20220104.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.nepplus.jickbangcopy_20220104.R
+import com.nepplus.jickbangcopy_20220104.ViewRoomDetailActivity
 import com.nepplus.jickbangcopy_20220104.models.RoomData
 import java.math.MathContext
 
@@ -34,8 +36,13 @@ class RoomAdapter(
 //              전체 한 줄을 표현하는 row변수에게 클릭 이벤트 처리
             row.setOnClickListener {
 //              임시 : 토스트 출력
-                Toast.makeText(mathContext, "${data.description}", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(mathContext, "${data.description}", Toast.LENGTH_SHORT).show()
 
+//              실제 : 상세보기 화면 이동 (Intent)
+                val myIntent = Intent(mathContext, ViewRoomDetailActivity::class.java)
+
+//                  화면의 도움을 받아 -> 다른 화면으로 이동 startActivity
+                mathContext.startActivity(myIntent)
 
             }
 

23cad80d2a534921c0e9765f05e83f0abcb480d3
Lee Seung Hun <negrat@naver.com>
2022-01-04 14:56:20 +0900

    방 데이터 표현용 클래스 제작

---
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/models/RoomData.kt
@@ -0,0 +1,9 @@
+package com.nepplus.jickbangcopy_20220104.models
+
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String, // 파이썬과 같이, 추가로 들어올 파라미터가 없어도 ,로 마무리해도 됨.
+) {
+}

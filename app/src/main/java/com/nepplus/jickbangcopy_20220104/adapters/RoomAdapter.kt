6908fda6b287baa9821aec4971b8ab35dc98f0c6
Lee Seung Hun <negrat@naver.com>
2022-01-04 15:00:35 +0900

    RommAdapter 클래스 제작 중     - inner class 생성 / 에러 해결

---
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -0,0 +1,12 @@
+package com.nepplus.jickbangcopy_20220104.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RoomAdapter : RecyclerView.Adapter<>() {
+
+    inner class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+}
